cf34edcde31c56e7b521873303e71cc27f7bd749
Cyril Roelandt <tipecaml@gmail.com>
2017-03-03 05:07:18 +0100

    Âge députés/sénateurs.

--- web.py
+++ web.py
@@ -3,9 +3,17 @@
 import csv
 import datetime
 import json
+import unicodedata
 
 data_maires = None
 data_maires_main = None
+data_sen_dep = None
+
+
+def remove_accents(s):
+    s = unicodedata.normalize('NFD', s)
+    s = s.encode('ascii', 'ignore').decode('ascii')
+    return s
 
 
 def get_mayor_data(name, firstname):
@@ -28,6 +36,30 @@ def get_mayor_data(name, firstname):
     raise ValueError('Could not find "%s %s"' % (name, firstname))
 
 
+def get_sen_dep_data(name, firstname):
+    name = remove_accents(name).upper()
+    firstname = remove_accents(firstname).upper()
+
+    # Some errors in the files provided by the government
+    if name == 'MASSON' and firstname == 'JEAN-LOUIS':
+        firstname = 'JEAN LOUIS'
+    if name == 'VOGEL' and firstname == 'JEAN-PIERRE':
+        firstname = 'JEAN PIERRE'
+
+    acteurs = data_sen_dep['export']['acteurs']['acteur']
+    for acteur in acteurs:
+        n = remove_accents(acteur['etatCivil']['ident']['nom']).upper()
+        p = remove_accents(acteur['etatCivil']['ident']['prenom']).upper()
+        if n == name and p == firstname:
+            birthdate = acteur['etatCivil']['infoNaissance']['dateNais']
+            birthdate = datetime.datetime.strptime(birthdate, '%Y-%m-%d')
+            return {
+                'birthdate': birthdate.date(),
+                'job': '99',
+            }
+    raise ValueError('Could not find "%s %s"' % (name, firstname))
+
+
 class Candidat(object):
     def __init__(self, name):
         self.name = name
@@ -35,6 +67,12 @@ class Candidat(object):
 
 
 class Soutien(object):
+    METHODS = {
+        'Maire': get_mayor_data,
+        'Sénateur/trice': get_sen_dep_data,
+        'Député-e': get_sen_dep_data,
+    }
+
     def __init__(self, name, firstname, mandate):
         self.name = name
         self.firstname = firstname
@@ -44,15 +82,16 @@ class Soutien(object):
         self.party = None
         self.job = None
 
-        if self.mandate == 'Maire':
+        method = self.METHODS.get(self.mandate)
+        if method is None:
+            self._fill_with_obvious_bullshit()
+        else:
             try:
-                mayor_data = get_mayor_data(self.name, self.firstname)
-                self.birthdate = mayor_data['birthdate']
-                self.job = mayor_data['job']
+                data = method(self.name, self.firstname)
+                self.birthdate = data['birthdate']
+                self.job = data['job']
             except ValueError:
                 self._fill_with_obvious_bullshit()
-        else:
-            self._fill_with_obvious_bullshit()
 
     def _fill_with_obvious_bullshit(self):
         self.birthdate = datetime.datetime(1900, 1, 1).date()
@@ -127,8 +166,15 @@ def read_data_mayors():
         data_maires_main = json.load(f)
 
 
+def read_data_sen_dep():
+    global data_sen_dep
+    with open('data/AMO20_dep_sen_min_tous_mandats_et_organes_XIV.json') as f:
+        data_sen_dep = json.load(f)
+
+
 def main():
     read_data_mayors()
+    read_data_sen_dep()
 
     with open('data/20170301-1.json') as f:
         p = json.load(f)
